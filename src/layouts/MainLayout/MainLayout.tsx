24173e9c5f49495e597e2fbf56e3b9801e54dfe9
hniht3002 <s2okaza2s@gmail.com>
2023-08-20 18:07:26 +0700

    construc layout + header + tippy

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,10 +1,27 @@
+import Header from '@/components/commonComponent/Header/Header';
 import {ReactNode} from 'react'
+
 function MainLayout({children}:{children: ReactNode}) {
     return (
-        <>
-            <h1>MainLayout</h1>
-            {children}
-        </>
+        <div className="wrapper">
+            <div className="header sticky top-0">
+                <Header/>
+            </div>
+            <div className="flex align-center m-auto w-5/6 gap-4 h-screen">
+                <div className="menu border-2 border-rose-400 flex-1">
+                    <h1>Menu</h1>
+                </div>
+
+                <div className="main border-2 border-rose-400 w-3/5">
+                    {children}
+                </div>
+
+                <div className="sidebar border-2 border-rose-400 flex-1">
+                    <h1>Sidebar</h1>
+                </div>
+
+            </div>
+        </div>
     );
 }
 
