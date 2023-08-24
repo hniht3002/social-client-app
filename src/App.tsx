3c456cadf5855bbb2f2629d5a75e411922151a15
hniht3002 <s2okaza2s@gmail.com>
2023-08-24 20:51:15 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
 import "@/App.css"
 import routes from "@/routes/routes"
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Redirect } from "react-router-dom";
+import { registered } from "./data/GlobalState";
 
 function App() {
-  return (
-    <div className="App">
-        <Routes>
-          {routes.map((route, index) => {
-            let Element = route.component;
-            let Layout = route.layout;
-            return (
-                <Route key={index} path={route.path} element = {<Layout> {<Element/>} </Layout>}></Route>
-            
-              )
-          })}
-        </Routes>
-    </div>
-  );
+  if(!registered) {
+    return <Redirect/>
+  } else {
+    return (
+      <div className="App">
+          <Routes>
+            {routes.map((route, index) => {
+              let Element = route.component;
+              let Layout = route.layout;
+              return (
+                  <Route key={index} path={route.path} element = {<Layout> {<Element/>} </Layout>}></Route>
+              
+                )
+            })}
+          </Routes>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
